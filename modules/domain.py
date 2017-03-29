4a642bd3f88851ba865e107742bc90677cb619f0
SrFlipFlop <ferran.obiols.colomina@gmail.com>
2017-03-29 23:06:05 +0200

    New module

---
+++ modules/domain.py
@@ -0,0 +1,36 @@
+from config import app
+from subprocess import Popen, PIPE
+
+import os
+
+@app.task
+def run(ip, project):
+	create_folder(project)
+	nmap_scripts(ip, project)
+
+def create_folder(project):
+	os.makedirs("{0}dns/".format(project))
+
+def nmap_scripts(ip, project):
+	ssh_scripts = [
+		"dns-blacklist.nse",
+		"dns-brute.nse",
+		"dns-cache-snoop.nse",
+		"dns-check-zone.nse",
+		"dns-client-subnet-scan.nse",
+		"dns-fuzz.nse",
+		"dns-nsec3-enum.nse",
+		"dns-nsec-enum.nse",
+		"dns-nsid.nse",
+		"dns-random-srcport.nse",
+		"dns-random-txid.nse",
+		"dns-recursion.nse",
+		"dns-service-discovery.nse",
+		"dns-srv-enum.nse",
+		"dns-zone-transfer.nse",
+	]
+
+	for script in ssh_scripts:
+		cmd = "nmap -sT --script={0} -oN {1}dns/nmap_{2}".format(script, project, script.replace('.nse', ''))
+		p = Popen(cmd, shell=True, stdout=PIPE, stdin=PIPE)
+		c = p.communicate()
