08bca63e43d45c7255ff659031b9596e67415d1d
SrFlipFlop <ferran.obiols.colomina@gmail.com>
2017-04-19 20:44:26 +0200

    New smtp module

---
+++ modules/smtp.py
@@ -0,0 +1,30 @@
+from config import app
+from subprocess import Popen, PIPE
+
+import os
+
+@app.task
+def run(ip, project):
+	create_folder(project)
+	nmap_scripts(ip, project)
+
+def create_folder(project):
+	os.makedirs("{0}smtp/".format(project))
+
+def nmap_scripts(ip, project):
+	ssh_scripts = [
+		"smtp-brute.nse",
+		"smtp-commands.nse",
+		"smtp-enum-users.nse",
+		"smtp-ntlm-info.nse",
+		"smtp-open-relay.nse",
+		"smtp-strangeport.nse",
+		"smtp-vuln-cve2010-4344.nse",
+		"smtp-vuln-cve2011-1720.nse",
+		"smtp-vuln-cve2011-1764.nse"
+	]
+
+	for script in ssh_scripts:
+		cmd = "nmap -sT --script={0} -oN {1}smtp/nmap_{2}".format(script, project, script.replace('.nse', ''))
+		p = Popen(cmd, shell=True, stdout=PIPE, stdin=PIPE)
+		c = p.communicate()
