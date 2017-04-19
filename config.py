74728bdb8804c34ebc6dfd08c169e76275bb7ddf
SrFlipFlop <ferran.obiols.colomina@gmail.com>
2017-04-19 23:48:07 +0200

    load modules celery

--- config.py
+++ config.py
@@ -1,3 +1,11 @@
 from celery import Celery
 
-app = Celery('nsa', broker='redis://localhost:6379/0')
+import os
+
+def load_modules():
+    modules = []
+    for root, dirs, files in os.walk('./modules'):
+        modules += map(lambda y: "modules.{0}".format(y.replace('.py', "")), filter(lambda x: '__' not in x and 'pyc' not in x, files))
+    return modules
+
+app = Celery('nsa', broker='redis://localhost:6379/0', include=load_modules())
